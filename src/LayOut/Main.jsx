14934657e9be88e7dd44db120dd7388ed5651608
tuhin_4ever <tuhin4ever19@gmail.com>
2023-06-07 03:16:34 +0600

    navbar added with logo

--- src/LayOut/Main.jsx
+++ src/LayOut/Main.jsx
@@ -1,10 +1,15 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../Shared/NavBar/NavBar";
 
 const Main = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <NavBar></NavBar>
+      <div className="min-h-[calc(100vh-110px)]">
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>this is Home</h2>
+      
+    </div>
+  );
+};
+
+export default Home;
