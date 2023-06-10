7dd298dc0fdabc2ad57dcb2cb710d2480399634c
tuhin_4ever <tuhin4ever19@gmail.com>
2023-06-11 02:41:00 +0600

    showing instructor student on instructor page

--- src/Pages/Home/PopularClassesSection/PopularClassesSection.jsx
+++ src/Pages/Home/PopularClassesSection/PopularClassesSection.jsx
@@ -10,7 +10,7 @@ const PopularClassesSection = () => {
   }, []);
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 ">
       <h2 className="text-2xl font-bold mb-4 text-center">Popular Classes</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {popularClasses.map((classItem) => (
@@ -23,7 +23,7 @@ const PopularClassesSection = () => {
               alt={classItem.name}
               className="w-full h-64 object-cover rounded-t-lg transition duration-300 ease-in-out transform hover:scale-105"
             />
-            <div className="px-4 mb-5 py-4 absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-center transition duration-300 ease-in-out opacity-0 hover:opacity-100">
+            <div className=" cursor-pointer px-4 mb-5 py-4 absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-center transition duration-300 ease-in-out opacity-0 hover:opacity-100">
               <h3 className="text-lg font-semibold mb-2 text-white">
                 {classItem.name}
               </h3>
--- src/Pages/Instructors/InstructorCard.jsx
+++ src/Pages/Instructors/InstructorCard.jsx
@@ -1,6 +1,6 @@
 import Tilt from "react-parallax-tilt";
 const InstructorCard = ({ item }) => {
-  const { name, image, email } = item;
+  const { name, image, email , students} = item;
   return (
     <div className="transition duration-300 transform hover:scale-105  rounded-lg overflow-hidden shadow-md">
       <div className="p-3">
@@ -15,6 +15,11 @@ const InstructorCard = ({ item }) => {
             </Tilt>
           </div>
           <h2 className="text-2xl font-bold mb-2 Kalam-text">{name}</h2>
+          {students && (
+            <p className="text-gray-600 mb-2 Permanent-text text-xl">
+              {students} Student{students > 1 ? "s" : ""}
+            </p>
+          )}
           <p className="text-gray-600 mb-2 Caveat-text text-3xl">{email}</p>
         </div>
       </div>
